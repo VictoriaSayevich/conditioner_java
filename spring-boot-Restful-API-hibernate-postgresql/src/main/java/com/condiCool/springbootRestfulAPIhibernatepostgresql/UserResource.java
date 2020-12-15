ad894d0bc1384e14db3b0caf83ac46b13d272705
Victoria Sayevich <victoria.sayevich@routine-automation.com>
2020-12-15 23:30:10 +0300

    feat: add post mapping login

--- spring-boot-Restful-API-hibernate-postgresql/src/main/java/com/condiCool/springbootRestfulAPIhibernatepostgresql/UserResource.java
+++ spring-boot-Restful-API-hibernate-postgresql/src/main/java/com/condiCool/springbootRestfulAPIhibernatepostgresql/UserResource.java
@@ -2,13 +2,28 @@ package com.condiCool.springbootRestfulAPIhibernatepostgresql;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 public class UserResource {
-    private Map<String, String> GenerateJWTToken(User user) {
+    @Autowired
+    UserController userController;
+
+    @PostMapping("/login")
+    public ResponseEntity<Map<String, String>> loginUser(@RequestBody Map<String, Object> userMap) {
+        String password = (String) userMap.get("password");
+        User user = userController.validateUser(password);
+        return new ResponseEntity<>(generateJWTToken(user), HttpStatus.OK);
+    }
+
+    private Map<String, String> generateJWTToken(User user) {
         long timestamp = System.currentTimeMillis();
         String token = Jwts.builder().signWith(SignatureAlgorithm.HS256, Constants.API_SECRET_KEY)
                 .setIssuedAt(new Date(timestamp))
