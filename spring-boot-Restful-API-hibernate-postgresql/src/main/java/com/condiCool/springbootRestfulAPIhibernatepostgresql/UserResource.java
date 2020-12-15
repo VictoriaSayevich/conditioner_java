a4ee007fe261198c8035f219a8420d84212e7364
Victoria Sayevich <victoria.sayevich@routine-automation.com>
2020-12-15 23:16:05 +0300

    feat: add generate JWT token method

--- spring-boot-Restful-API-hibernate-postgresql/src/main/java/com/condiCool/springbootRestfulAPIhibernatepostgresql/UserResource.java
+++ spring-boot-Restful-API-hibernate-postgresql/src/main/java/com/condiCool/springbootRestfulAPIhibernatepostgresql/UserResource.java
@@ -1,2 +1,23 @@
-package com.condiCool.springbootRestfulAPIhibernatepostgresql;public class UserResource {
+package com.condiCool.springbootRestfulAPIhibernatepostgresql;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+public class UserResource {
+    private Map<String, String> GenerateJWTToken(User user) {
+        long timestamp = System.currentTimeMillis();
+        String token = Jwts.builder().signWith(SignatureAlgorithm.HS256, Constants.API_SECRET_KEY)
+                .setIssuedAt(new Date(timestamp))
+                .setExpiration(new Date(timestamp + Constants.TOKEN_VALIDITY))
+                .claim("userId", user.getUserId())
+                .claim("userLogin", user.getLogin())
+                .compact();
+        Map<String, String> map = new HashMap<>();
+        map.put("token", token);
+        return map;
+    }
 }
