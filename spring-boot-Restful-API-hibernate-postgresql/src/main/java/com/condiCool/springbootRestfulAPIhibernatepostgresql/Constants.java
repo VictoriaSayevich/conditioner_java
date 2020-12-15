1a5b2b1d8bcca4341995b5198eb34e8173cf0cf2
Victoria Sayevich <victoria.sayevich@routine-automation.com>
2020-12-15 23:16:59 +0300

    add: key constants

--- spring-boot-Restful-API-hibernate-postgresql/src/main/java/com/condiCool/springbootRestfulAPIhibernatepostgresql/Constants.java
+++ spring-boot-Restful-API-hibernate-postgresql/src/main/java/com/condiCool/springbootRestfulAPIhibernatepostgresql/Constants.java
@@ -1,2 +1,7 @@
-package com.condiCool.springbootRestfulAPIhibernatepostgresql;public class Constants {
+package com.condiCool.springbootRestfulAPIhibernatepostgresql;
+
+public class Constants {
+    public static final String API_SECRET_KEY = "adminCoolCondiKey";
+    public static  final long TOKEN_VALIDITY = 2 * 60 * 60 * 1000;
+
 }
