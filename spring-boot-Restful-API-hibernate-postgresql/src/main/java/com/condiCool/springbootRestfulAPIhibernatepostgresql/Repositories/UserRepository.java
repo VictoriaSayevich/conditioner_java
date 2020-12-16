8e460d910c3cfdef245005589d2d96593222b0f5
Victoria Sayevich <victoria.sayevich@routine-automation.com>
2020-12-16 20:15:10 +0300

    fix: add query

--- spring-boot-Restful-API-hibernate-postgresql/src/main/java/com/condiCool/springbootRestfulAPIhibernatepostgresql/Repositories/UserRepository.java
+++ spring-boot-Restful-API-hibernate-postgresql/src/main/java/com/condiCool/springbootRestfulAPIhibernatepostgresql/Repositories/UserRepository.java
@@ -1,16 +1,33 @@
 package com.condiCool.springbootRestfulAPIhibernatepostgresql.Repositories;
 
 import com.condiCool.springbootRestfulAPIhibernatepostgresql.Entity.UserEntity;
-import com.condiCool.springbootRestfulAPIhibernatepostgresql.Exceptions.AuthException;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
-public interface UserRepository {
+import java.util.Optional;
 
-    Integer create(String name, String login, String password) throws AuthException;
 
-    UserEntity findByLoginAndPassword(String login, String password) throws AuthException;
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
 
+//    Integer create(String name, String login, String password);
+//
+//    UserEntity findByLoginAndPassword(String login, String password);
+//
+//    Integer getCountByLogin(String login);
+//
+//    UserEntity findById(Integer userId);
+
+    @Query(value = "SELECT id, name, login, password FROM USERS WHERE login = ?1", nativeQuery = true)
+    UserEntity findByLoginAndPassword(String login, String password);
+
+    @Query(value = "SELECT COUNT(*) FROM USERS WHERE login = ?1", nativeQuery = true)
     Integer getCountByLogin(String login);
 
-    UserEntity findById(Integer userId);
+    @Query(value = "SELECT id, name, login, password FROM USERS WHERE id = ?1", nativeQuery = true)
+    public UserEntity findUserById(Integer userId);
 
+    @Query(value = "INSERT INTO USERS (id, name, login, password) VALUES (DEFAULT, ?1, ?2, ?3) returning USERS.id", nativeQuery = true)
+    public Integer create(String name, String login, String password);
 }
