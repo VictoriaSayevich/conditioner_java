5ab5b840c170b45a30b0f72d285926acfc04acf9
Victoria Sayevich <victoria.sayevich@routine-automation.com>
2020-12-16 11:29:36 +0300

    add: package resources

--- spring-boot-Restful-API-hibernate-postgresql/src/main/java/com/condiCool/springbootRestfulAPIhibernatepostgresql/Resources/UserResource.java
+++ spring-boot-Restful-API-hibernate-postgresql/src/main/java/com/condiCool/springbootRestfulAPIhibernatepostgresql/Resources/UserResource.java
@@ -1,5 +1,8 @@
-package com.condiCool.springbootRestfulAPIhibernatepostgresql;
+package com.condiCool.springbootRestfulAPIhibernatepostgresql.Resources;
 
+import com.condiCool.springbootRestfulAPIhibernatepostgresql.Constants;
+import com.condiCool.springbootRestfulAPIhibernatepostgresql.Controller.UserController;
+import com.condiCool.springbootRestfulAPIhibernatepostgresql.Entity.UserEntity;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,11 +10,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
+@RestController
+@RequestMapping("/api/user")
 public class UserResource {
     @Autowired
     UserController userController;
@@ -21,8 +28,8 @@ public class UserResource {
         String login = (String) userMap.get("login");
         String password = (String) userMap.get("password");
 
-        User user = userController.validateUser(login, password);
-        return new ResponseEntity<>(generateJWTToken(user), HttpStatus.OK);
+        UserEntity userEntity = userController.validateUser(login, password);
+        return new ResponseEntity<>(generateJWTToken(userEntity), HttpStatus.OK);
     }
 
     @PostMapping ("/register")
@@ -31,17 +38,17 @@ public class UserResource {
         String login = (String) userMap.get("login");
         String password = (String) userMap.get("password");
 
-        User user = userController.registerUser(name, login, password);
-        return new ResponseEntity<>(generateJWTToken(user), HttpStatus.OK);
+        UserEntity userEntity = userController.registerUser(name, login, password);
+        return new ResponseEntity<>(generateJWTToken(userEntity), HttpStatus.OK);
     }
 
-    private Map<String, String> generateJWTToken(User user) {
+    private Map<String, String> generateJWTToken(UserEntity userEntity) {
         long timestamp = System.currentTimeMillis();
         String token = Jwts.builder().signWith(SignatureAlgorithm.HS256, Constants.API_SECRET_KEY)
                 .setIssuedAt(new Date(timestamp))
                 .setExpiration(new Date(timestamp + Constants.TOKEN_VALIDITY))
-                .claim("userId", user.getUserId())
-                .claim("userLogin", user.getLogin())
+                .claim("userId", userEntity.getUserId())
+                .claim("userLogin", userEntity.getLogin())
                 .compact();
         Map<String, String> map = new HashMap<>();
         map.put("token", token);
